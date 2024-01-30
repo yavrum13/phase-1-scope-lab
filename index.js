49150c845155563aca089bd5b1f2f1ba12640347
yavrum13 <yuval.avrum@gmail.com>
2024-01-29 23:36:43 -0600

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Write your solution in this file!
+
+var customerName = 'bob';
+
+function upperCaseCustomerName(){
+    customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer(){
+    bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer(){
+    bestCustomer = 'maybe bob';
+}
+
+const leastFavoriteCustomer = 'definitely roger';
+
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = 'maybe not roger, not sure anymore'
+}
+console.log(changeLeastFavoriteCustomer());
